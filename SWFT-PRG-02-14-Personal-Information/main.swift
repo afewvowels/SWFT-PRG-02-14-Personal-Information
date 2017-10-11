c6e8a429caca4bd9d5d896ed0e9b24ebab8ef689
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 08:35:45 -0400

    Initial Commit

---
+++ SWFT-PRG-02-14-Personal-Information/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-PRG-02-14-Personal-Information
+//
+//  Created by Keith Smith on 10/11/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
